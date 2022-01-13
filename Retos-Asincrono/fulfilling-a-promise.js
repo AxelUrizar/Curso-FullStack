a2415e0cca297381e08d5d1711c885759c02c87d
AxelUrizar <axelurizarmartin@gmail.com>
2022-01-13 10:25:09 +0100

    minor fix

--- Retos-Asincrono/fulfilling-a-promise.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-    var promise = new Promise(function (fulfill, reject) {
-      setTimeout(fulfill, 300, 'FULFILLED!')
-    });
-
-    promise.then(console.log);
-    // promise.then((param) => {console.log(param)})
--- Retos-Asincrono/rejecting-a-promise.js
+++
@@ -1,9 +0,0 @@
-var promise = new Promise(function (fulfill, reject) {
-    setTimeout(() => reject(new Error('REJECTED!')), 300);
-  });
-
-  function onReject (error) {
-    return console.log(error.message)
-  }
-
-promise .then(() => console.log('fulfill'), onReject);
