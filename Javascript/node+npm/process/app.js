78e506f33afdb61399c436c34f7e21746f46d5e1
AxelUrizar <axelurizarmartin@gmail.com>
2022-01-18 20:17:42 +0100

    cosas

---
+++ Javascript/node+npm/process/app.js
@@ -0,0 +1,19 @@
+"use strict"
+
+//versión de node actual
+
+console.log(process.version);
+
+//versión de node actual
+
+console.log(process.memoryUsage());
+
+//canto tiempo ha tardado en ejecutarse este script
+
+console.log (process.uptime());
+
+//finalizamos
+
+process.exit(0);
+
+console.log("It doesn't come down here")
